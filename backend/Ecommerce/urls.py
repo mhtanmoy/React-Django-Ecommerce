f6232ce3e65b9f123b5b0cef35ce3bccafc930a4
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-06-04 23:39:26 +0600

    serializers added

--- backend/Ecommerce/urls.py
+++ backend/Ecommerce/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('users/login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.home, name="home"),
+    path('users', views.getUsers, name="users"),
     path('users/profile', views.getUserProfile, name="user-profile"),
     path('products/', views.Products, name="products"),
     path('products/<str:pk>', views.getProduct),
--- backend/Ecommerce/views.py
+++ backend/Ecommerce/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .Serializers import *
 from .models import *
@@ -9,9 +10,17 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer= UserSerializer(users, many=True)
     return Response(serializer.data)
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -29,6 +38,8 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 def home(request):
     return JsonResponse('Hi', safe=False)
+
+
 
 @api_view(['GET'])
 def Products(request):
