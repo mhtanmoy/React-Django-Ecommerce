8a9b4d65a73b231ac984adde820cf91642b1228a
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-06-04 22:29:30 +0600

    token serializer added

--- backend/Ecommerce/views.py
+++ backend/Ecommerce/views.py
@@ -4,14 +4,23 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .Serializers import *
 from .models import *
+from django.contrib.auth.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
-        data['username'] = self.user.username
-        data['email'] = self.user.email
+
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
 
         return data
 
