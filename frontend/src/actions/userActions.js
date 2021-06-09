82d75aeb6331df4d10d5927d343736a19be3ee5d
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-06-09 12:42:50 +0600

    update profile added

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,14 +2,21 @@ import {
     USER_LOGIN_REQUEST, 
     USER_LOGIN_SUCCESS, 
     USER_LOGIN_FAIL, 
-    USER_LOGOUT ,
+    USER_LOGOUT,
+
     USER_REGISTER_REQUEST, 
     USER_REGISTER_SUCCESS, 
     USER_REGISTER_FAIL,
+
     USER_DETAILS_REQUEST, 
     USER_DETAILS_SUCCESS, 
     USER_DETAILS_FAIL,
     USER_DETAILS_RESET,
+
+    USER_UPDATE_PROFILE_REQUEST, 
+    USER_UPDATE_PROFILE_SUCCESS, 
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_RESET,
 } from '../constants/userConstants'
 import axios from 'axios'
 
@@ -127,6 +134,53 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
     }catch(error){
         dispatch({
             type: USER_DETAILS_FAIL,
+            payload: error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message,
+        })
+    }
+}
+
+
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+    try{
+        dispatch({
+            type: USER_UPDATE_PROFILE_REQUEST
+        })
+
+        const {
+            userLogin: {userInfo},
+        } = getState()
+
+        const config = {
+            headers:{
+                'Content-type':'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.put(
+            '/api/users/update',
+            user,
+            config
+            )
+
+        dispatch({
+            type:USER_UPDATE_PROFILE_SUCCESS,
+            payload:data
+        })
+
+        dispatch({
+            type:USER_LOGIN_SUCCESS,
+            payload:data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+
+    }catch(error){
+        dispatch({
+            type: USER_UPDATE_PROFILE_FAIL,
             payload: error.response && error.response.data.detail
             ? error.response.data.detail
             : error.message,
--- frontend/src/screen/ProfileScreen.js
+++ frontend/src/screen/ProfileScreen.js
@@ -5,8 +5,8 @@ import Loader from '../components/Loader'
 import Message from '../components/Message'
 import FormContainer from '../components/FormContainer'
 import { useDispatch, useSelector } from 'react-redux'
-import { getUserDetails } from '../actions/userActions'
-
+import { getUserDetails, updateUserProfile } from '../actions/userActions'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
  
 function ProfileScreen({ history }) {
     const [name, setName] = useState('')
@@ -23,25 +23,35 @@ function ProfileScreen({ history }) {
     const userLogin = useSelector(state => state.userLogin)
     const {userInfo} = userLogin
 
+    const userUpdateProfile = useSelector(state => state.userUpdateProfile)
+    const { success } = userUpdateProfile
+
     useEffect(() => {
         if (!userInfo){
             history.push('/login')
         }else{
-            if(!user || !user.name){
+            if(!user || !user.name || success){
+                dispatch({ type:USER_UPDATE_PROFILE_RESET })
                 dispatch(getUserDetails('profile'))
             }else{
                 setName(user.name)
                 setEmail(user.email)
             }
         }
-    }, [dispatch ,history, userInfo, user])
+    }, [dispatch ,history, userInfo, user, success])
 
     const submitHandler = (e) => {
         e.preventDefault()
         if( password != confirmPassword){
             setMessage('Password do not match')
         }else{
-            console.log('updating..')
+            dispatch(updateUserProfile({ 
+                'id': user._id,
+                'name': name,
+                'email': email,
+                'password': password
+             }))
+             setMessage('')
         } 
     }
     return (
