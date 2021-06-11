88befbe301d882693f8635fee0227aae1640f8e7
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-06-11 22:25:24 +0600

    added total

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import RegisterScreen from './screen/RegisterScreen'
 import ProfileScreen from './screen/ProfileScreen'
 import ShippingScreen from './screen/ShippingScreen'
 import PaymentScreen from './screen/PaymentScreen'
+import PlaceOrderScreen from './screen/PlaceOrderScreen'
 import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 
@@ -23,6 +24,7 @@ function App() {
         <Route path='/register' component={RegisterScreen} />
         <Route path='/profile' component={ProfileScreen} />
         <Route path='/payment' component={PaymentScreen} />
+        <Route path='/placeorder' component={PlaceOrderScreen} />
         <Route path='/shipping' component={ShippingScreen} />
         <Route path='/product/:id' component={ProductScreen} />
         <Route path='/cart/:id?' component={CartScreen} />
