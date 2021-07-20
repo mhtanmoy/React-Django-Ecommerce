adab074986a606d268e95a907ee6e85e897a9be4
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-07-20 21:41:38 +0600

    final project

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+import os
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -97,7 +97,9 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -171,7 +173,8 @@ MEDIA_URL = '/images/'
 
 
 STATICFILES_DIRS=[
-    BASE_DIR/ 'static'
+    BASE_DIR/ 'static',
+    BASE_DIR / 'frontend/build/static'
 ]
 
 MEDIA_ROOT='static/images'
