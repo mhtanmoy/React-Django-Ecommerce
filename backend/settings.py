dc97699d62931bc60a09d2e1060a934c122145b3
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2021-08-25 21:09:54 +0600

    added static root

--- backend/settings.py
+++ backend/settings.py
@@ -166,7 +166,11 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+import os
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
